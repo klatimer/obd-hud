ef15088b189746ab32a32587f9676450f7d2f7ad
Ken <guitarsrockalot@gmail.com>
2017-08-28 03:03:59 +0000

    added backlight on/off functionality

--- lcd_test.py
+++ lcd_test.py
@@ -16,6 +16,8 @@ lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
 			   lcd_columns, lcd_rows, lcd_backlight)
 
 # Print a two line message
+lcd.set_backlight(1)
 lcd.message('Hello\nworld!')
-time.sleep(5.0)
+time.sleep(3.0)
+lcd.set_backlight(0)
 lcd.clear()
