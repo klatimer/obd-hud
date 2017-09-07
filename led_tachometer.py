13353df447c666139d3f85c6ba51952b45c0fe1c
Ken <guitarsrockalot@gmail.com>
2017-09-07 23:56:59 +0000

    fixed bug with list indexing

--- led_tachometer.py
+++ led_tachometer.py
@@ -6,22 +6,23 @@
 import RPi.GPIO as GPIO
 import time
 
-class LED_Tachometer:
+class tachometer:
 	def __init__(self, led_pins, redline):
 		self.led_pins = led_pins
 		self.num_leds = len(led_pins)
 		self.current_rpm = 0
-		self.max_rpm = int(1000 * num_leds) # Using 1 LED per 1k RPM (for now)
+		self.max_rpm = int(1000 * self.num_leds) # Using 1 LED per 1k RPM (for now)
 		# Set up the GPIO
 		GPIO.setmode(GPIO.BCM)
 		for pin in led_pins:
 			GPIO.setup(pin, GPIO.OUT)
 
 	def display_rpm(self, current_rpm):
-		num_leds_on = int(round(current_rpm))
+		led_pins = self.led_pins
+		num_leds_on = int(round(current_rpm / 1000.0))
 		# Turn on the proper number of LEDs
 		for i in range(num_leds_on):
-			GPIO.output(self.led_pins(i), GPIO.HIGH)
+			GPIO.output(led_pins[i], GPIO.HIGH)
 
 	def clear_rpm(self):
 		for pin in self.led_pins:
