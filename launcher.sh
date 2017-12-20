bb6fb008c63ddd084eae0ddd5e497ea9dba9ee09
Ken <guitarsrockalot@gmail.com>
2017-12-20 00:11:45 +0000

    added shell script to run python program at startup

---
+++ launcher.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# launcher.sh
+# navigate to home, then to this directory, then run a python script
+# make sure to edit /etc/rc.local as root and add the following:
+# 	su - pi -c "bash /home/pi/path-to-this-file/launcher.sh &"
+
+cd /
+cd ~/python/obd_hud
+sudo python led_init.py
+cd /
