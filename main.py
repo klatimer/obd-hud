951a3bc4490106dac994bbaff4dbcd149b6199b5
Ken <guitarsrockalot@gmail.com>
2017-12-23 18:39:48 -0800

    fixed bugs in lcd thread

--- main.py
+++ main.py
@@ -40,9 +40,9 @@ class lcd_thread(threading.Thread):
 		lcd_backlight = 21
 		lcd_columns = 16
 		lcd_rows = 2
-		lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
+		self.lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
 			   lcd_columns, lcd_rows, lcd_backlight)
-		lcd.set_backlight(1)
+		self.lcd.set_backlight(1)
 		threading.Thread.__init__(self)
 		self._running = True
 	def run(self):
@@ -51,18 +51,18 @@ class lcd_thread(threading.Thread):
 		while True:
 			try:
 				if connection.status == OBDStatus.CAR_CONNECTED:
-					lcd.message('%s %s' % ('RPM:\n', rpm))
+					self.lcd.message('%s %s' % ('RPM:\n', rpm))
 				else:
-					lcd.message('Connecting...')
+					self.lcd.message('Connecting...')
 				time.sleep(0.25)
 			except RuntimeError:
-				lcd.message('Error')
+				self.lcd.message('Error')
 				pass
 
 if __name__ == "__main__":
 	connection = obd.OBD() # auto connect
-	# thread_2 = lcd_thread()
-	# thread_2.start()
+	thread_2 = lcd_thread()
+	thread_2.start()
 	while connection.status == obd.OBDStatus.NOT_CONNECTED:
 		time.sleep(0.1)
 	thread_1 = led_thread()
